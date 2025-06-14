255dadfd68beaebb7e64948774d610e2be6e1fc8
Abhinav Gupta <71538723+abgdev02@users.noreply.github.com>
2025-06-13 21:39:59 -0400

    UI Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // GitHub Pages sub-path
-  base: '/landing-page-v2/',
+  // Serve from root for development
+  base: '/',
 
   plugins: [react()],
 
