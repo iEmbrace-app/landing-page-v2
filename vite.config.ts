6fa161d2ff82c7d7021b880dccc6f0e8e1fe4458
Abhinav Gupta <71538723+abgdev02@users.noreply.github.com>
2025-07-13 13:26:39 -0400

    updated vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   // Serve from root for development
-  base: '/',
+  base: '/landing-page-v2/',
 
   plugins: [
     react(),
